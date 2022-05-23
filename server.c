c512ac8686eb90016e8425b99d35ccceefbe9155
abschill <ajay198810@gmail.com>
2022-05-22 20:24:14 -0600

    update: length global

--- server.c
+++ server.c
@@ -14,6 +14,7 @@
 #define EOL "\r\n"
 #define EOL_SIZE 4
 #define BACKLOG 10   // how many pending connections queue will hold
+size_t fsize = 580;
 
 void sigchld_handler(int s) {
     // waitpid() might overwrite errno, so we save and restore it:
@@ -23,15 +24,10 @@ void sigchld_handler(int s) {
 }
 
 char* get_home_content() {
-  char* buffer; 
+  char* buffer;
   long length;
-  // char success[] = "HTTP/1.1 200 OK \r\n";
-  // open the index html in read mode
   FILE * f = fopen("index.html", "r");
   if(f) {
-    //append success msg before writing the file itself
-     
-    //strcat(buffer, success);
     /**
       fseek sets the file position of the stream to the given offset.
       it starts at the back, so we have to set SEEK_END as the final parameter as the "from" argument
@@ -43,13 +39,13 @@ char* get_home_content() {
      * ftell returns the current file position of the specified stream with respect ot the start of the 	file. this basically gives us the total size after moving the pointer to the end of the file
      **/
     length = ftell(f);
-    //strncat(buffer, 
+    //set the global response length to the size of the file bc it has the headers
+    fsize = (size_t)length;
     // seek set moves the pointer to the beginning of the file like we did earlier with SEEK_END
     fseek(f, 0, SEEK_SET);
-    // set the buffer for the length of the string
-    //strcpy(buffer
+    // set the buffer size for the length of the file
     buffer = malloc(length);
-    // if it exists read the content into the buffer
+    // make sure its pointing to the allocation space (non-null)
     if(buffer) {
       // read file into buffer
       fread(buffer, 1, length, f);
@@ -57,17 +53,8 @@ char* get_home_content() {
     // close the pointer before returning
     fclose(f);
   }
-  // return the buffer as a char* (make sure u free after giving to the client)
-  //printf("Content: \n%s\n", buffer);
-  //strcat(buffer, "Content Length: 
+  // return char* buffer
   return buffer;
-}
-
-struct addrinfo  set_hints() {
-    struct addrinfo hints;
-    
-   
-    return hints;
 }
 
 // get sockaddr, IPv4 or IPv6:
@@ -79,25 +66,14 @@ void *get_in_addr(struct sockaddr *sa) {
     return &(((struct sockaddr_in6*)sa) -> sin6_addr);
 }
 
-void ensure_addr(struct addrinfo *a) {
-  if (a == NULL)  {
-        fprintf(stderr, "server: failed to bind\n");
-        exit(1);
-   }
-}
-// convert the http response body into a valid http request with the headers and zlib integration (todo)
-//char* msg_to_http(char* msg) {
-  
-  //}
-
 int main() {
     int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
     struct addrinfo hints, *servinfo, *p;
     struct sockaddr_storage req_addr; // request address 
     socklen_t sin_size;
     struct sigaction sa;
-    int yes=1;
     char s[INET6_ADDRSTRLEN];
+    int y = 1;
     int rv;
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
@@ -110,14 +86,15 @@ int main() {
 
     // loop through all the results and bind to the first we can
     for (p = servinfo; p != NULL; p = p-> ai_next) {
-        if ((sockfd = socket(p-> ai_family, p-> ai_socktype,
-                p-> ai_protocol)) == -1) {
+        if ((sockfd = socket(p-> ai_family, p-> ai_socktype, p-> ai_protocol)) == -1) {
             perror("server: socket");
             continue;
         }
 
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                sizeof(int)) == -1) {
+        // setsockopt() arg (socket, level, oname, oval, olen)
+        // socketfd, protocol specified by level int (SOL_SOCKET means its specified in the stream itself)
+        // &1 is the address of the flag to set the "reuse" address option to true
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &y, sizeof(int)) == -1) {
             perror("setsockopt");
             exit(1);
         }
@@ -130,14 +107,16 @@ int main() {
         break;
     }
 
-    freeaddrinfo(servinfo); // all done with this structure
+    freeaddrinfo(servinfo); // all done with this
 
-    //ensure_addr(p);
+    if (p == NULL)  {
+        fprintf(stderr, "server: failed to bind\n");
+        exit(1);
+   }
     if (listen(sockfd, BACKLOG) == -1) {
         perror("listen");
         exit(1);
     }
-    
     sa.sa_handler = sigchld_handler; // reap all dead processes
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
@@ -160,24 +139,21 @@ int main() {
             get_in_addr((struct sockaddr *)&req_addr),
             s, sizeof s);
         printf("server: got connection from %s\n", s);
-	char* content = get_home_content();
-	//send(new_fd, content, 500, 0);
+	    char* content = get_home_content();
         if (!fork()) { // this is the child process
             close(sockfd); // child doesn't need the listener
-	    //char* content = get_home_content();
-	    //char* content_200 = create_200_content(raw_content);
-	    //char* response_data;
-	    //strcat("HTTP/1.1 200 OK\r\n", content);
-	    //	    sprintf("%s\n", content);
-	   
-	    // printf("Content Size: %ld\n", sizeof content_200);
-            if (send(new_fd, content, 1200, 0) == -1)
-                perror("send");
-            close(new_fd);
-	    free(content);
-            exit(0);
+
+            char http200[19] = "HTTP/1.1 200 OK\r\n";
+            // if(send(new_fd, http200, 19, 0) == -1) {
+            //     perror("appending 200 hdr");
+            // }
+            if (send(new_fd, content, fsize, MSG_CONFIRM) == -1)
+                    perror("send");
+                close(new_fd);
+                exit(0);
         }
-        close(new_fd);  // parent doesn't need this
+        free(content);
+        close(new_fd);  // close after
 	//free(content);
     }
 
